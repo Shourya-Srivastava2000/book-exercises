57c861ec35bf28a92acdd2c32dd956a7d48e81c8
Shourya-Srivastava2000 <55703732+Shourya-Srivastava2000@users.noreply.github.com>
2020-02-16 20:20:41 -0800

    Add files via upload

--- exercise.R
+++ exercise.R
@@ -1,61 +1,111 @@
-# Exercise 1: reading and querying a web API
+# Exercise 2: advanced ggplot2 practice
 
-# Load the httr and jsonlite libraries for accessing data
-# You can also load `dplyr` if you wish to use it
-library("httr")
-library("jsonlite")
-library("dplyr")
+# Install and load the `ggplot2` package
+# install.packages('ggplot2')
+library("ggplot2")
 
-# Create a variable base_uri that stores the base URI (as a string) for the 
-# Github API (https://api.github.com)
-base_uri <- "https://api.github.com"
+# For this exercise you will again be working with the `diamonds` data set.
+# Use `?diamonds` to review details about this data set
+?diamonds
 
-# Under the "Repositories" category of the API documentation, find the endpoint 
-# that will list _repos in an organization_. Then create a variable named
-# `org_resource` that stores the endpoint for the `programming-for-data-science`
-# organization repos (this is the _path_ to the resource of interest).
-org_resource <- "/orgs/programming-for-data-science/repos"
+## Position Adjustments
 
-# Send a GET request to this endpoint (the `base_uri` followed by the 
-# `org_resource` path). Print the response to show that your request worked. 
-# (The listed URI will also allow you to inspect the JSON in the browser easily).
-response <- GET(paste0(base_uri, org_resource))
-print(response)
+# Draw a column (bar) chart of diamonds cuts by price, with each bar filled by 
+# clarity. You should see a _stacked_ bar chart.
+ggplot(data = diamonds) +
+  geom_col(mapping = aes(x = cut, y = price, fill = clarity))
 
-# Extract the content of the response using the `content()` function, saving it
-# in a variable.
-response_text <- content(response, "text")
+# Draw the same chart again, but with each element positioned to "fill" the y axis
+ggplot(data = diamonds) +
+  geom_col(mapping = aes(x = cut, y = price, fill = clarity), position = "fill")
 
-# Convert the content variable from a JSON string into a data frame.
-org_repos <- fromJSON(response_text)
+# Draw the same chart again, but with each element positioned to "dodge" each other
+ggplot(data = diamonds) +
+  geom_col(mapping = aes(x = cut, y = price, fill = clarity), position = "dodge")
 
-# How many (public) repositories does the organization have?
-print(nrow(org_repos))
+# Draw a plot with point geometry with the x-position mapped to `cut` and the
+# y-position mapped to `clarity`
+# This creates a "grid" grouping the points
+ggplot(data = diamonds) +
+  geom_point(mapping = aes(x = cut, y = clarity))
 
-# Now a second query:
-# Create a variable `search_endpoint` that stores the endpoint used to search 
-# for repositories. (Hint: look for a "Search" endpoint in the documentation).
-search_endpoint <- "/search/repositories"
+# Use the "jitter" position adjustment to keep the points from all overlapping!
+# (This works a little better with a sample of diamond data, such as from the
+# previous exercise).
+ggplot(data = diamonds) +
+  geom_point(mapping = aes(x = cut, y = clarity), position = "jitter")
 
-# Search queries require a query parameter (for what to search for). Create a 
-# `query_params` list variable that specifies an appropriate key and value for 
-# the search term
-query_params <- list(q = "graphics")
 
-# Send a GET request to the `search_endpoint`--including your params list as the
-# `query`. Print the response to show that your request worked.
-response <- GET(paste0(base_uri, search_endpoint), query = query_params)
-print(response)
+## Scales
 
-# Extract the content of the response and convert it from a JSON string into a
-# data frame. 
-response_text <- content(response, "text")
-graphics_repos = fromJSON(response_text)
+# Draw a "boxplot" (with `geom_boxplot`) for the diamond's price (y) by color (x)
+ggplot(data = diamonds) +
+  geom_boxplot(mapping = aes(x = color, y = price))
 
-# How many search repos did your search find? (Hint: check the list names to 
-# find an appropriate value).
-print(graphics_repos$total_count)
+# This has a lot of outliers, making it harder to read. To fix this, draw the
+# same plot but with a _logarithmic_ scale for the y axis.
+ggplot(data = diamonds) +
+  geom_boxplot(mapping = aes(x = color, y = price)) +
+  scale_y_log10()
 
-# What are the full names of the top 5 repos in the search results?
-graphics_repo_names <- graphics_repos$items$full_name[1:5]
-print(graphics_repo_names)
+# For another version, draw the same plot but with `violin` geometry instead of
+# `boxplot` geometry!
+# How does the logarithmic scale change the data presentation?
+ggplot(data = diamonds) +
+  geom_violin(mapping = aes(x = color, y = price)) +
+  scale_y_log10()
+
+# Another interesting plot: draw a plot of the diamonds price (y) by carat (x),
+# using a heatmap of 2d bins (geom_bin2d)
+# What happens when you make the x and y channels scale logarithmically?
+ggplot(data = diamonds) +
+  geom_bin2d(mapping = aes(x = carat, y = price)) +
+  scale_x_log10() +
+  scale_y_log10()
+
+# Draw a scatter plot for the diamonds price (y) by carat (x). Color each point
+# by the clarity (Remember, this will take a while. Use a sample of the diamonds
+# for faster results)
+ggplot(data = diamonds) +
+  geom_point(mapping = aes(x = carat, y = price, color = clarity))
+
+# Change the color of the previous plot using a ColorBrewer scale of your choice.
+# What looks nice?
+ggplot(data = diamonds) +
+  geom_point(mapping = aes(x = carat, y = price, color = clarity)) +
+  scale_color_brewer(palette = "Spectral")
+
+
+## Coordinate Systems
+
+# Draw a bar chart with x-position and fill color BOTH mapped to cut
+# For best results, SET the `width` of the geometry to be 1 (fill plot, no space
+# between)
+# TIP: You can save the plot to a variable for easier modifications
+bar <- ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x = cut, fill = cut), width = 1)
+bar
+
+# Draw the same chart, but with the coordinate system flipped
+bar + coord_flip()
+
+# Draw the same chart, but in a polar coordinate system. It's a Coxcomb chart!
+bar + coord_polar()
+
+
+## Facets
+
+# Take the scatter plot of price by carat data (colored by clarity) and add
+# _facets_ based on the diamond's `color`
+ggplot(data = diamonds) +
+  geom_point(mapping = aes(x = carat, y = price, color = clarity)) +
+  scale_color_brewer(palette = "Spectral") +
+  facet_wrap(~ color)
+
+
+## Saving Plots
+
+# Use the `ggsave()` function to save the current (recent) plot to disk.
+# Name the output file "my-plot.png".
+# Make sure you've set the working directory!!
+ggsave("my-plot.png")
